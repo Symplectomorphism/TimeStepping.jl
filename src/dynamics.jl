222cb288056637a6434c87f12d88446f888d42ba
Aykut C. Satici <acsatici@gmail.com>
2021-01-31 22:04:41 -0700

    finish the code tomorrow

--- src/dynamics.jl
+++ src/dynamics.jl
@@ -7,12 +7,13 @@ mutable struct Dynamics
     qM::Array{Float64, 1}
     qE::Array{Float64, 1}
     uE::Array{Float64, 1}
-    W::Array{Float64, 2}
-    Λ::Array{Float64, 1}
-    g::Function
+    W::Array{Float64, 2}            # Jacobian transpose in the normal direction
+    Λ::Array{Float64, 1}            # Normal contact force
+    g::Function                     # Gap function
+    ε::Float64                      # Coefficient of restitution (normal dir.)
 end
 
 
-function Dynamics()
-    
+function Dynamics(M::Array{Float64, 2}, h::Array{Float64, 1})
+
 end
