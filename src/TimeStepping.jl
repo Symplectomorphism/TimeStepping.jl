b0e9638a3c982075cf5f74b48782e36c95c98bfd
Aykut C. Satici <acsatici@gmail.com>
2021-02-14 20:14:54 -0700

    progress on integrator

--- src/TimeStepping.jl
+++ src/TimeStepping.jl
@@ -5,8 +5,10 @@ using JuMP
 using PATHSolver
 using Ipopt
 using LinearAlgebra
+using Revise
 
 include("extra_file.jl")
+include("moreau.jl")
 
 export my_f, derivative_of_my_f
 
--- src/moreau.jl
+++ src/moreau.jl
@@ -3,6 +3,9 @@ mutable struct Moreau
     gap::Function                   # Gap function -- provide the force matrices
     M::Array{Float64, 2}
     h::Array{Float64, 1}
+    tA::Float64
+    tM::Float64
+    tE::Float64
     qA::Array{Float64, 1}
     uA::Array{Float64, 1}
     qM::Array{Float64, 1}
@@ -21,19 +24,30 @@ n: degrees of freedom of the system
 m: number of unilateral constraints
 """
 
-function Moreau(n::Int, gap::Function, dynamics::Function)
-    qA = zeros(Float64, n)
-    uA = zeros(Float64, n)
-    qM = zeros(Float64, n)
+function Moreau(n::Int, gap::Function, dynamics::Function, q::Vector, u::Vector)
+    Δt = 1e-3
+    qA = q
+    uA = u
+    qM = _compute_mid_displacements(qA, uA, Δt)
+    M, h = dynamics(qM, uA)
     qE = zeros(Float64, n)
     uE = zeros(Float64, n)
-    M, h = dynamics(qA, uA)
-    Δt = 1e-3
+    tA, tM, tE = zeros(Float64, 3)
+    tM = _compute_mid_time(tA, Δt)
+    tE = _compute_mid_time(tM, Δt)
     ε = 0.5
     g, W = gap(qA, uA)
-    H = zeros(Int, 0)
+    H = zeros(Int, 1)
     Λ = zeros(Float64, length(H))
 
-    Moreau(dynamics, gap, M, h, qA, uA, qM, qE, uE, g, W, Λ, H, Δt, ε)
+    Moreau(dynamics, gap, M, h, tA, tM, tE, qA, uA, qM, qE, uE, g, W, Λ, H, Δt, ε)
 end
 
+
+function _compute_mid_time(t::Number, Δt::Number)
+    return t + 1/2*Δt
+end
+
+function _compute_mid_displacements(q::Vector, u::Vector, Δt::Number)
+    return q + 1/2*Δt*u
+end
